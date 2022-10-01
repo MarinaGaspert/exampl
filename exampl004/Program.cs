3c2de2d58d99cd536687d3dc194d5cda35458ae3
Marina <gaspertrishka@mail.ru>
2022-10-01 16:28:40 +0700

    ex4

--- exampl004/Program.cs
+++ exampl004/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numderA = 12;
+double numderB = 5;
+Console.WriteLine(numderA / numderB);
